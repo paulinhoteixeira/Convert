be5cf9f7a8827ee9786e37e1abd2892dfec37f89
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-09-19 20:25:16 -0300

    get input amount

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const amount = document.getElementById("amount")
+
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "")
+})
+
